0fdcdf428d68087e3174c4b9ae14770eb99e3ab2
=-kriss-= <kriss@modelcitizen.com.au>
2014-03-25 16:31:50 +1100

    fixing dogey error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
 
   def index
+    raise params.inspect
     if !session[:address]
       @address = request.location
-      if @address.include? "Geocoder"
+      if @address.longitude == '0.0' && @address.latitude == '0.0'
         #default address to get around testing environment
         @address = "608 Harris St, Sydney"
       else
